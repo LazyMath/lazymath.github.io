d5b9ef0923ab59f2468316129b0151045c29c713
OpenSourceEdu2023 <149875899+OpenSourceEdu2023@users.noreply.github.com>
2024-01-28 22:17:15 +1100

    changes

---
+++ _app/immutable/chunks/store.6cbc4a4d.js
@@ -0,0 +1,1 @@
+import{w as s}from"./index.7f2c929d.js";const a=s({svg_id:"/moon.svg",logo_id:"/logo-light.svg",compass_id:"/compass-light.svg"}),g=s(["Loading chatbot ..."]);export{a as d,g as m};
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-const disp_data = writable({
-    "svg_id": "/moon.svg",
-    "logo_id": "/logo-light.svg",
-    "compass_id": "/compass-light.svg",
-})
-export const messages = writable(["Loading chatbot ..."]);
-
-export default disp_data;
