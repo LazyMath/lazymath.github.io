8f6b8f2a24b178553537be629d58300e7accc1fe
OpenSourceEdu2023 <149875899+OpenSourceEdu2023@users.noreply.github.com>
2024-01-29 22:52:42 +1100

    changes

--- _app/immutable/chunks/stores.3803d409.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.466c0a51.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
