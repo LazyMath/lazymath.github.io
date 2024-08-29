cd166327d417baffe8ba5800d452054fed8984d1
kykz77 <149875899+LazyMath@users.noreply.github.com>
2024-08-30 09:40:34 +1000

    Updated changelog

---
+++ _app/immutable/chunks/stores.a7811392.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.a7fa9a15.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
